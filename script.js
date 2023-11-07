0d396f33fe1bf0d89d9b3b512360b644ef7a2e0b
Solkol <61247142+Solkol@users.noreply.github.com>
2023-11-07 15:16:33 +0100

    Add operate()

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ let firstOperand;
 let secondOperand;
 let operator;
 
+function operate(operation, firstNumber, secondNumber) {
+    switch (operation) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
+    }
+}
+
 function add(x, y) {
     return x + y;
 }
